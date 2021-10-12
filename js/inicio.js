4588c85c08f48f94d7213493c364db983060e48d
redmedicavital <40214841+redmedicavital@users.noreply.github.com>
2021-10-12 17:27:51 -0500

    login-e-inicio

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,24 @@
 const getUrl = new URLSearchParams(window.location.search);
-const id = getUrl.get('id');
-console.log(id);
+const url = getUrl.get('URL');
+const urlAlbumes = "albums";
+let albumes = [];
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let name = data.username;
+        document.getElementById("nameUser").innerText = `${name}`
+    })
+    .catch(err => console.log(err))
+
+async function obtAlbum(url) {
+    let res = await fetch(url);
+    let data = await res.json();
+    return data;
+}
+
+async function obtDatos() {
+    albumes = await obtAlbum(`${url}/${urlAlbumes}`)
+    console.log(albumes);
+}
+
+obtDatos();
